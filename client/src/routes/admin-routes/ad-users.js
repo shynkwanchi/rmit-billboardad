4b0385e205f40691c85fc37d36211121b33c84f3
big-kent <s3863969@rmit.edu.vn>
2022-09-12 21:21:27 +0700

    fix ad-users

--- client/src/routes/admin-routes/ad-users.js
+++ client/src/routes/admin-routes/ad-users.js
@@ -1,52 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getResponse } from "../../middleware/response";
 import { LoadUsers } from "../../middleware/load-data";
 import "./admin-panel.css";
 
 export default function Users() {
 
-    const [data, setData] = useState([]);
-    const [name, setName] = useState('');
-    const [usersname, setUsersname] = useState('');
-    const [email, setEmail] = useState('');
-    const [passwords, setPassword] = useState('');
-    const [gender, setGender] = useState('');
-    const [phone, setPhone] = useState('');
-    const users = LoadUsers();
-    const URL = 'http://localhost:5000/pages/';
+    const [human, setHuman] = useState({
+        id: '',
+        name: '',
+        usersname: '',
+        email: '',
+        password: '',
+        gender: '',
+        phone: '',
+    });
+    const{_id}= useParams();
 
-    const load = () => {
-        fetch(URL)
-            .then(res => res.json())
-            .then(json => setData(json))
-    }
     useEffect(() => {
-        load()
-    }, [])
+        fetch(`http://localhost:5000/pages/${_id}`)
+        .then(res => res.json())
+        .then(data => setHuman(data))
+    }, [_id]);
 
-    const save = () => {
-        if (name === '') {
-            fetch(URL, {
-                method: "post",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ name: name, username: usersname, email: email, password: passwords, gender: gender, phone: phone })
-            })
-                .then(res => {
-                    console.log(res)
-                    load()
-                })
-        }
-
-        else {
-            fetch(URL, {
+    const save = (e, id) => {
+        e.preventDefault();
+        try {
+            fetch(`http://localhost:5000/pages/${id}`, {
                 method: "put",
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ name: name, username: usersname, email: email, password: passwords, gender: gender, phone: phone })
+                body: JSON.stringify({ name: human.name, email: human.email, phone: human.phone })
             })
                 .then(res => {
                     console.log(res)
@@ -55,38 +40,46 @@ export default function Users() {
         }
     }
 
-    const edit = (password) => {
-        setPassword(passwords)
-        setUsersname(usersname)
-    }
-    
 
-    
-    return (
-        <>
-            <h1>Users</h1>
+    const deleteUsers = (id) => {
+            fetch(URL + "/" + id, {
+                method: "delete"
+            })
+                .then(res => {
+                    console.log(res)
+                    const newdata = data.filter((item) => {
+                        return item._id != id
+                    })
+                    setData(newdata)
+                })
+        }
 
-            <div className="row filters">
-                <div className="col-6 col-sm-4 col-md-3 filter-container">
-                    Time Range
-                    <select className="form-select" aria-label="Select billboard type">
-                        <option defaultValue="0">Lastest</option>
-                        <option value="1">Oldest</option>
-                        <option value="2">Most Billboard</option>
-                        <option value="3">Most Order</option>
-                    </select>
+        const editUsers = ({ password: human.password, username: human.usersname })
+        return (
+            <div>
+                <h1>Users</h1>
+
+                <div className="row filters">
+                    <div className="col-6 col-sm-4 col-md-3 filter-container">
+                        Time Range
+                        <select className="form-select" aria-label="Select billboard type">
+                            <option defaultValue="0">Lastest</option>
+                            <option value="1">Oldest</option>
+                            <option value="2">Most Billboard</option>
+                            <option value="3">Most Order</option>
+                        </select>
+                    </div>
+
+                    <div className="col-6 col-sm-4 col-md-3 filter-container">
+                        Name Search
+                        <input type="text" placeholder="Name here..." className="form-control" aria-label="Select billboard type" />
+                        <ul className="listdata">
+                            {/* need a data list of user to input here this one is just a static filter */}
+                        </ul>
+                    </div>
+
+
                 </div>
-
-                <div className="col-6 col-sm-4 col-md-3 filter-container">
-                    Name Search
-                    <input type="text" placeholder="Name here..." className="form-control" aria-label="Select billboard type" />
-                    <ul className="listdata">
-                        {/* need a data list of user to input here this one is just a static filter */}
-                    </ul>
-                </div>
-
-
             </div>
-        </>
-    )
-}
+        )
+    }
