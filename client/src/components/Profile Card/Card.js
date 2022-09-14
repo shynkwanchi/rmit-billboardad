6430465f828820d0b56fa9b591879fcab169b796
big-kent <s3863969@rmit.edu.vn>
2022-09-14 13:05:09 +0700

    still css

--- client/src/components/Profile Card/Card.js
+++ client/src/components/Profile Card/Card.js
@@ -4,23 +4,26 @@ import Card from "react-bootstrap/Card";
 import ModalContact from "../Modal Form Contact/ContactForm";
 
 const ProfileCard = () => {
-  const cardDetail = 
-    {
-      image: "https://i.insider.com/50f967f56bb3f7830a000019",
-      title: "Lebron James",
-      text: "THE GOAT",
-    }
+  const cardDetail =
+  {
+    image: "https://i.insider.com/50f967f56bb3f7830a000019",
+    title: "Lebron James",
+    text: "THE GOAT",
+  }
 
-    return (
+  return (
+    <div>
       <Card style={{ width: "18rem" }} className="box">
         <Card.Img variant="top" src={cardDetail.image} />
         <Card.Body>
           <Card.Title>{cardDetail.title}</Card.Title>
           {/* <Card.Text>{card.text}</Card.Text> */}
-          <ModalContact/>
+          <ModalContact />
         </Card.Body>
       </Card>
-    );
-  };
+      <Card />
+    </div>
+  );
+};
 
 export default ProfileCard;
